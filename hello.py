7af078c11335997096d3f466187761717080cbb5
omerkhn <omerkhn98@gmail.com>
2021-02-02 13:54:51 -0800

    Updated to test git

---
+++ hello.py
@@ -0,0 +1,14 @@
+# this program says hello and asks for my name
+
+print ('hello world!')
+print ('what is your name?') # ask for their name
+myName = input()
+print ('it is good to meet you, ' + myName)
+print ('the lenth of your name is:')
+print (len(myName))
+
+print ('what is your age?') # ask for their age
+myAge = input()
+print('you will be ' + str(int(myAge) + 1) + ' in a year ')
+
+# edits to test git
